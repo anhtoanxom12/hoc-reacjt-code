08cf6bde51ef56bbc8d8386dd197b44e9c454c19
demo <anhtoanxom12@gmai.com>
2022-09-13 18:19:13 +0700

    update

---
+++ B3.js
@@ -0,0 +1,7 @@
+let index = 0;
+let n = 3;
+for (let i =0; i <= n; i++){
+    index +=i+2;
+
+}
+console.log(index);
---
+++ B4.js
@@ -0,0 +1,6 @@
+let n = 5;
+let sum = 0;
+for (let i =1;i <= n; i+=2){
+    sum += i
+}
+console.log(sum);
